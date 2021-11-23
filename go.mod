73105ec90ecdc86609565c44c75f9df25a4d98a6
Paul Rothrock <paul@movetoiceland.com>
2021-11-23 16:23:39 -0500

    Recording editedBy in Props
    Signed-off-by: Paul Rothrock <paul@movetoiceland.com>

--- go.mod
+++ go.mod
@@ -1,8 +1,9 @@
-module github.com/mattermost/mattermost-plugin-starter-template
+module github.com/icelander/mattermost-compliance-enhancements
 
 go 1.16
 
 require (
+	github.com/mattermost/mattermost-server/v5 v5.39.2 // indirect
 	github.com/mattermost/mattermost-server/v6 v6.0.0-20210906125346-b41b7eae1026
 	github.com/pkg/errors v0.9.1
 	github.com/stretchr/testify v1.7.0
--- server/plugin.go
+++ server/plugin.go
@@ -1,11 +1,10 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
 	"sync"
 
 	"github.com/mattermost/mattermost-server/v6/plugin"
+	"github.com/mattermost/mattermost-server/v6/model"
 )
 
 // Plugin implements the interface expected by the Mattermost server to communicate between the server and plugin processes.
@@ -20,9 +19,33 @@ type Plugin struct {
 	configuration *configuration
 }
 
-// ServeHTTP demonstrates a plugin that handles HTTP requests by greeting the world.
-func (p *Plugin) ServeHTTP(c *plugin.Context, w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello, world!")
-}
+func (p *Plugin) MessageWillBeUpdated(c *plugin.Context, newPost, oldPost *model.Post) (*model.Post, string) {
+	configuration := p.getConfiguration()
 
-// See https://developers.mattermost.com/extend/plugins/server/reference/
+	if configuration.disabled {
+		return newPost, ""
+	}
+
+	// When editing a post, the userID for newPost and oldPost are the same so we look it up from the session
+	currentSession, err := p.API.GetSession(c.SessionId)
+
+	if err != nil {
+		p.API.LogError("Could not get session, not updating editedBy value", "Session ID:", c.SessionId, "Error", err)
+		return newPost, ""
+	}
+
+	updatingUserId := ""
+	// If the post is being edited and the person editing isn't the original person
+	if (newPost.UserId != currentSession.UserId) {
+		p.API.LogInfo("Updating EditedBy Field", "updatingUserId", updatingUserId, "newPost.UserId", newPost.UserId)
+		updatingUserId = currentSession.UserId	
+	} else {
+		updatingUserId = newPost.UserId
+	}
+	
+	postProps := newPost.GetProps()
+	postProps["editedBy"] = updatingUserId
+	newPost.SetProps(postProps)
+	
+	return newPost, ""
+}
